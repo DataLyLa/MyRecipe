aed212190236d21fa697a554de3b55562b2d0a76
Maher <amri.site@yahoo.com>
2023-05-28 17:18:07 +0200

    Modification in carousel and the bootstrap in index.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  onclicknext() {
+  slides: any[] = new Array(3).fill({
+    id: -1,
+    src: '',
+    title: '',
+    subtitle: '',
+  });
+  constructor() {}
 
+  ngOnInit(): void {
+    this.slides[0] = {
+      id: 0,
+      src: '../../assets/cake.jpeg',
+      title: 'Rainbow cake',
+      subtitle: 'try this recipe !',
+    };
+    this.slides[1] = {
+      id: 1,
+      src: '../../assets/grilled-meat.jpeg',
+      title: 'Grilled meat',
+      subtitle: 'try this recipe !',
+    };
+    this.slides[2] = {
+      id: 2,
+      src: '../../assets/pasta.jpeg',
+      title: 'Pasta',
+      subtitle: 'try this recipe !',
+    };
   }
-
 }
