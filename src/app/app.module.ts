e87a441ceade37ed29fe0800fea5497f0c86f8a0
Hasan Emir TUKOZOGLU <tukozogluster@gmail>
2023-06-01 09:43:45 +0200

    Service Category Header AJoutés

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    CategoryPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor() { }
+
+
+  myFunction() {
+    const navbarNav = document.getElementById("navbarNav");
+    if (navbarNav) {
+      navbarNav.classList.toggle("show");
+    }
+  }
+
+  // Fermer le menu déroulant si l'utilisateur clique en dehors de celui-ci
+  windowOnClick(event: MouseEvent) {
+    if (!(event.target instanceof Element) || !event.target.matches('.navbar-toggler')) {
+      const dropdowns = document.getElementsByClassName("collapse navbar-collapse");
+      for (let i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  ngOnInit() {
+    window.addEventListener('click', this.windowOnClick);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('click', this.windowOnClick);
+  }
+
 }
