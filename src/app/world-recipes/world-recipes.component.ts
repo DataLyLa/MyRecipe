bec31bc7f5d2bbd986d847f0c73927a5f27abe9c
Hasan Emir TUKOZOGLU <tukozogluster@gmail>
2023-06-14 14:00:12 +0200

    Dernière modification sur le moteur de recherche

--- src/app/world-recipes/world-recipes.component.ts
+++ src/app/world-recipes/world-recipes.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CategoryService } from '../services/category.service';
 import { Router } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { FLAGS } from '../flags';
 
 
 @Component({
@@ -15,6 +16,8 @@ export class WorldRecipesComponent implements OnInit {
   nationalityList: any[] = [];
   @Input() choice: string = "";
   showErrorMessage: boolean = false;
+  flags = FLAGS;
+  title = 'icon-flag';
 
 
   constructor(private categoryService: CategoryService, private router: Router) { }
@@ -31,14 +34,19 @@ export class WorldRecipesComponent implements OnInit {
 
   goTORecepiesByCountry(country: string) {
     if (this.choice && this.choice.trim() !== '') {
-    this.router.navigate(["all-recipes", country])
-  } else {
-    this.showErrorMessage = true;
-  }
+      this.router.navigate(["all-recipes", country])
+    } else {
+      this.showErrorMessage = true;
+    }
   }
 
-  getIconPath(area: string): string {
-    return `../../assets/icons/${area}.ico`;
+  getFlag(area: string) {
+    for (let flag of this.flags) {
+      if (area === flag.name) {
+        return flag.emoji;
+      }
+    }
+    return null;
   }
 
 }
